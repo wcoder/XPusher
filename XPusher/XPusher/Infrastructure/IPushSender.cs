8a605f650fdefd0d20958461e7c9ce4cc19590fa
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-01-18 12:51:09 +0300

    Added sender for android

---
+++ XPusher/XPusher/Infrastructure/IPushSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace XPusher.Infrastructure
+{
+	public interface IPushSender
+	{
+		string Name { get; }
+
+		Task<string> SendMessageAsync(string apiKey, string message);
+	}
+}
---
+++ XPusher/XPusher/Services/AndroidPushSender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using XPusher.Infrastructure;
+
+namespace XPusher.Services
+{
+	public class AndroidPushSender : IPushSender
+	{
+		private const string GcmUrl = "https://gcm-http.googleapis.com/gcm/send";
+
+		public string Name => nameof(AndroidPushSender);
+
+		public async Task<string> SendMessageAsync(string apiKey, string message)
+		{
+			var jData = new JObject {{"message", message}};
+			var jGcmData = new JObject {{"to", "/topics/global"}, {"data", jData}};
+			var content = new StringContent(jGcmData.ToString(), Encoding.UTF8, "application/json");
+
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"key={apiKey}");
+
+					var response = await client.PostAsync(new Uri(GcmUrl), content);
+					return await response.Content.ReadAsStringAsync();
+				}
+			}
+			catch (Exception e)
+			{
+				return $"Error: {e.Message}";
+			}
+		}
+	}
+}
